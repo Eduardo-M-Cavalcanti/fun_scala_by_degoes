19cc706d8f267d253cec7e4f472ab34c4a78a664
Eduardo M. Cavalcanti <eduardo.m.cavalcanti@uol.com.br>
2018-09-09 12:50:02 -0300

    functional scala by John De Goes

---
+++ src/main/scala/net/degoes/02-abstractions/exercises.scala
@@ -0,0 +1,199 @@
+// Copyright(C) 2018 - John A. De Goes. All rights reserved.
+
+package net.degoes.abstractions
+
+import scalaz._
+import Scalaz._
+
+object algebra {
+  //
+  // EXERCISE 1
+  //
+  // Define a semigroup for `NotEmpty` below.
+  //
+  case class NotEmpty[+A](head: A, tail: Option[NotEmpty[A]])
+  implicit def NotEmptySemigroup[A]: Semigroup[NotEmpty[A]] =
+    ???
+  val example1 = NotEmpty(1, None) |+| NotEmpty(2, None)
+
+  //
+  // EXERCISE 2
+  //
+  // Design a permission system for securing some resource, together with a
+  // monoid for the permission data structure.
+  //
+  case class Permission(/* ??? */)
+  implicit val MonoidPermission: Monoid[Permission] = ???
+  val example2 = mzero[Permission] |+| Permission()
+
+  //
+  // EXERCISE 3
+  //
+  // Define an instance of `Semigroup` for `(A, B)` when both `A` and
+  // `B` form semigroups.
+  //
+  implicit def SemigroupTuple2[A: Semigroup, B: Semigroup]:
+    Semigroup[(A, B)] = ???
+
+  //
+  // EXERCISE 4
+  //
+  // Try to define an instance of `Monoid` for `NotEmpty` for any type `A`.
+  //
+  implicit def MonoidNotEmpty[A]: Monoid[NotEmpty[A]] = ???
+}
+
+object functor {
+  //
+  // EXERCISE 1
+  //
+  // Define an instance of `Functor` for `BTree`.
+  //
+  sealed trait BTree[+A]
+  case class Leaf[A](a: A) extends BTree[A]
+  case class Fork[A](left: BTree[A], right: BTree[A]) extends BTree[A]
+  implicit val BTreeFunctor: Functor[BTree] =
+    new Functor[BTree] {
+      def map[A, B](fa: BTree[A])(f: A => B): BTree[B] =
+        ???
+    }
+
+  //
+  // EXERCISE 2
+  //
+  // Define an instance of `Functor` for `Nothing`.
+  //
+  implicit val NothingFunctor: Functor[Nothing] = ???
+
+  //
+  // EXERCISE 3
+  //
+  // Define an instance of `Functor` for Parser[E, ?].
+  //
+  case class Parser[+E, +A](run: String => Either[E, (String, A)])
+  implicit def ParserFunctor[E]: Functor[Parser[E, ?]] = ???
+
+  //
+  // EXERCISE 4
+  //
+  // Try to define an instance of `Functor` for the following data type.
+  //
+  case class DataType[A](f: A => A)
+  implicit val DataTypeFunctor: Functor[DataType] = ???
+
+  //
+  // EXERCISE 5
+  //
+  // Define an instance of `Functor` for `FunctorProduct`.
+  //
+  case class FunctorProduct[F[_], G[_], A](l: F[A], r: F[A])
+  implicit def FunctorProductFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorProduct[F, G, ?]] = ???
+
+  //
+  // EXERCISE 6
+  //
+  // Define an instance of `Functor` for `FunctorSum`.
+  //
+  case class FunctorSum[F[_], G[_], A](run: Either[F[A], G[A]])
+  implicit def FunctorSumFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorSum[F, G, ?]] = ???
+
+  //
+  // EXERCISE 7
+  //
+  // Define an instance of `Functor` for `FunctorNest`.
+  //
+  case class FunctorNest[F[_], G[_], A](run: F[G[A]])
+  implicit def FunctorNestFunctor[F[_]: Functor, G[_]: Functor]:
+    Functor[FunctorNest[F, G, ?]] = ???
+
+  //
+  // EXERCISE 8
+  //
+  // Define `Applicative` for `Option`.
+  //
+  implicit val OptionApplicative: Applicative[Option] =
+    new Applicative[Option] {
+      def point[A](a: => A): Option[A] = ???
+
+      def ap[A, B](fa: => Option[A])(f: => Option[A => B]): Option[B] =
+        ???
+    }
+
+  //
+  // EXERCISE 9
+  //
+  // Implement `zip` in terms of the applicative composition using `|@|`.
+  //
+  val example1 = (Option(3) |@| Option(5))((_, _))
+  val example2 = zip(Option(3), Option("foo")) : Option[(Int, String)]
+  def zip[F[_]: Applicative, A, B](l: F[A], r: F[B]): F[(A, B)] =
+    ???
+  def ap2[F[_]: Applicative, A, B](fa: F[A], fab: F[A => B]): F[B] =
+    ???
+
+  //
+  // EXERCISE 10
+  //
+  // Define an instance of `Applicative` for `Parser[E, ?]`.
+  //
+  implicit def ApplicativeParser[E]: Applicative[Parser[E, ?]] =
+    new Applicative[Parser[E, ?]] {
+      def point[A](a: => A): Parser[E,A] = ???
+
+      def ap[A, B](fa: => Parser[E,A])(
+        f: => Parser[E, A => B]): Parser[E,B] = ???
+    }
+
+  //
+  // EXERCISE 11
+  //
+  // Define an instance of `Monad` for `BTree`.
+  //
+  implicit val MonadBTree: Monad[BTree] = ???
+
+  //
+  // EXERCISE 12
+  //
+  // Define an instance of `Monad` for `Parser[E, ?]`.
+  //
+  implicit def MonadParser[E]: Monad[Parser[E, ?]] = ???
+}
+
+object foldable {
+  //
+  // EXERCISE 1
+  //
+  // Define an instance of `Foldable` for `BTree`.
+  //
+  sealed trait BTree[+A]
+  case class Leaf[A](a: A) extends BTree[A]
+  case class Fork[A](left: BTree[A], right: BTree[A]) extends BTree[A]
+
+  //
+  // EXERCISE 2
+  //
+  // Try to define an instance of `Foldable` for `A => ?`.
+  //
+  implicit def FunctionFoldable[A]: Foldable[A => ?] = ???
+
+  //
+  // EXERCISE 3
+  //
+  // Define an instance of `Traverse` for `BTree`.
+  //
+  implicit val TraverseBTree: Traverse[BTree] = ???
+
+  //
+  // EXERCISE 4
+  //
+  // Try to define an instance of `Traverse` for `Parser[E, ?]`.
+  //
+  case class Parser[+E, +A](run: String => Either[E, (String, A)])
+  implicit def TraverseParser[E]: Traverse[Parser[E, ?]] = ???
+}
+
+object optics {
+
+}
